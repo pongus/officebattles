e29abd06c2146ae9f1e053d9969445d453f00da5
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-09-17 00:13:29 +0200

    companies: added companies to admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Company
+
+class CompanyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name',    {'fields': ['name']}),
+        ('Created', {'fields': ['created'], 'classes': ['collapse']}),
+    ]
+    list_filter = ['created']
+    search_fields = ['name']
+
+admin.site.register(Company, CompanyAdmin)
