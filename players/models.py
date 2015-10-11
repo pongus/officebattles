1972b6a7c738988b1e919ae599b4eabe5e744f78
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-10-12 00:04:32 +0200

    Extended user model with company, office and games fields

--- players/models.py
+++ players/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Player(models.Model):
+    user = models.OneToOneField(User)
+    company = models.ForeignKey('companies.Company')
+    office = models.ForeignKey('offices.Office')
+    games = models.ManyToManyField('games.Game')
