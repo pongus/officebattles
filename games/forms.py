b1ee8181da611732a6efaee919cb9b5ac587fa13
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-10-11 13:06:30 +0200

    Updated adding games form to include game mode options

---
+++ games/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+GAME_MODES = (
+    (0, '1V1'),
+    (1, '2V2'),
+    (2, '3V3'),
+    (3, '4V4'),
+    (4, '5V5'),
+    (5, 'FFA'),
+)
+
+class AddGameForm(forms.Form):
+    name = forms.CharField(label='Game name', max_length=128)
+    mode = forms.ChoiceField(choices=GAME_MODES)
