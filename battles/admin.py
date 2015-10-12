f875020e0c03c2c6a439b6d8523ba7496c5ffaf1
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-10-13 00:13:36 +0200

    Added battle and score models and admins

--- battles/admin.py
+++ battles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Battle, Result
+
+class BattleAdmin(admin.ModelAdmin):
+    fields = ['game', 'players']
+    list_display = ('game', 'created', 'updated')
+    list_filter = ['game', 'players', 'created', 'updated']
+    search_fields = ['game', 'players']
+
+admin.site.register(Battle, BattleAdmin)
+
+class ResultAdmin(admin.ModelAdmin):
+    fields = ['battle', 'player', 'rank', 'score']
+    list_display = ('battle', 'player', 'rank', 'score', 'created', 'updated')
+    list_filter = ['battle', 'player', 'created', 'updated']
+    search_fields = ['battle', 'player']
+
+admin.site.register(Result, ResultAdmin)
--- battles/models.py
+++ battles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Battle(models.Model):
+    game = models.ForeignKey('games.Game')
+    players = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.game) + ' Battle'
+
+class Result(models.Model):
+    battle = models.ForeignKey('battles.Battle')
+    player = models.ForeignKey(User)
+    rank = models.PositiveSmallIntegerField(null=True)
+    score = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.player) + ', Rank: ' + str(self.rank) + ', Score: ' + str(self.score)
