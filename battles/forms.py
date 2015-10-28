9e5523f82032a955170e7265460ceb750bfac17b
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-10-28 23:44:32 +0100

    Continued work for adding battles...

--- battles/forms.py
+++ battles/forms.py
@@ -1,8 +1,9 @@
-from games.models import Game
-from django.contrib.auth.models import User
-
 from django import forms
 
-class AddBattleForm(forms.Form):
-    game = forms.ModelChoiceField(queryset=Game.objects.all())
-    players = forms.ModelChoiceField(queryset=User.objects.all())
+from .models import Battle
+
+class BattleForm(forms.ModelForm):
+
+    class Meta:
+        model = Battle
+        fields = ['game', 'players']
--- battles/views.py
+++ battles/views.py
@@ -6,45 +6,78 @@ from offices.models import Office
 from games.models import Game
 from battles.models import Battle
 
-from battles.forms import AddBattleForm
+from .forms import BattleForm
 
 # Create your views here.
-def add(request, company_id, office_id, game_id):
-    office = get_object_or_404(Office, pk=office_id)
-    game = get_object_or_404(Game, pk=game_id)
+def create(request):
+    # office = get_object_or_404(Office, pk=office_id)
+    # game = get_object_or_404(Game, pk=1)
 
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        add_battle_form = AddBattleForm(request.POST)
-        # check whether it's valid:
-        if add_battle_form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/')
+        form = BattleForm(request.POST)
 
-    # if a GET (or any other method) we'll create a blank form
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            # return HttpResponseRedirect('blog.views.post_detail', pk=post.pk)
+
     else:
-        add_battle_form = AddBattleForm()
+        form = BattleForm()
 
-    context = {
-        'office': office,
-        'game': game,
-        'form': add_battle_form,
-    }
+    return render(request, 'battles/create.html', {'form': form})
 
-    return render(request, 'battles/add.html', context)
+def edit(request, battle_id):
+    battle = get_object_or_404(Battle, pk=battle_id)
 
-def save(request, company_id, office_id, game_id):
-    battle_game = request.POST.get('game')
-    # battle_players = request.POST.get('players')
-    battle_updated = timezone.now()
-    battle = Battle(
-        game_id = battle_game,
-        # players = 1,
-        updated = battle_updated
-    )
-    battle.save()
+    if request.method == "POST":
+        form = BattleForm(request.POST, instance=battle)
 
-    return HttpResponseRedirect('/company/' + str(company_id) + '/office/' + str(office_id) + '/game/' + str(game_id) + '/battle/add')
+        if form.is_valid():
+            battle = form.save(commit=False)
+            battle.save()
+
+            # return HttpResponseRedirect('blog.views.post_detail', pk=post.pk)
+
+        else:
+            form = BattleForm(instance=battle)
+
+        return HttpResponseRedirect('battle/edit.html', {'form': form})
+
+
+
+    # # if this is a POST request we need to process the form data
+    # if request.method == 'POST':
+    #     # create a form instance and populate it with data from the request:
+    #     battle_form = BattleForm(request.POST)
+    #     # check whether it's valid:
+    #     if battle_form.is_valid():
+    #         # process the data in form.cleaned_data as required
+    #         # ...
+    #         # redirect to a new URL:
+    #         return HttpResponseRedirect('/')
+
+    # # if a GET (or any other method) we'll create a blank form
+    # else:
+    #     battle_form = BattleForm()
+
+    # context = {
+    #     'office': office,
+    #     'game': game,
+    #     'form': battle_form,
+    # }
+
+    # return render(request, 'battles/edit.html', context)
+
+# def save(request, company_id, office_id, game_id):
+#     battle_game = request.POST.get('game')
+#     # battle_players = request.POST.get('players')
+#     battle_updated = timezone.now()
+#     battle = Battle(
+#         game_id = battle_game,
+#         # players = 1,
+#         updated = battle_updated
+#     )
+#     battle.save()
+
+#     return HttpResponseRedirect('/company/' + str(company_id) + '/office/' + str(office_id) + '/game/' + str(game_id) + '/battle/add')
