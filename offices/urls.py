3160d442def59de37a62de43d1d0b8225a0cdc61
Patfred <fredrikssonpatric@gmail.com>
2015-11-06 22:49:14 +0100

    removed unneeded template

--- offices/urls.py
+++ offices/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
-    url(r'^(?P<company_id>[0-9]+)/office/add$', views.add, name='add'),
-    url(r'^(?P<company_id>[0-9]+)/office/save$', views.save, name='save'),
-    url(r'^(?P<company_id>[0-9]+)/office/list$', views.list, name='list'),
-    url(r'^(?P<company_id>[0-9]+)/office/(?P<office_id>[0-9]+)$', views.view, name='view'),
-]
+#urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    #url(r'^list$', views.list, name='list'),
+   # url(r'^new/$', views.office_new, name='office_new'),
+  #  url(r'^(?P<office_id>[0-9]+)$', views.office_view, name='office_view'),
+ #   url(r'^office/(?P<office_id>[0-9]+)/edit/$', views.office_edit, name='office_edit'),
+#]
