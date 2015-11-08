ad5ecee05e56e5eee451592300e4776850bc6d97
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-11-08 14:38:55 +0100

    Added basic template structure

--- officebattles/urls.py
+++ officebattles/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
     url(r'^company/', include('companies.urls', namespace="company")),
     url(r'^company/', include('offices.urls', namespace="office")),
     url(r'^company/', include('games.urls', namespace="game")),
