9e8025e02cc1f3a9f1d72505e2a2a1bb934a54b6
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2015-11-17 00:34:12 +0100

    Added settings for media directory

--- officebattles/settings.py
+++ officebattles/settings.py
@@ -109,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
