d7dc8a413da539e0ea21a4b1f7fe9e4ad82e9d8d
Pontus Gustafsson <pontus.d.gustafsson@gmail.com>
2016-01-16 00:54:14 +0100

    Updated admin as well with the new field name

--- games/admin.py
+++ games/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Game
 
+
 class GameAdmin(admin.ModelAdmin):
-    fields = ['office', 'name', 'mode', 'min_players', 'max_players', 'coin_toss']
+    fields = ['office', 'name', 'mode', 'min_players', 'max_players', 'has_coin_toss']
     list_display = ('name', 'mode', 'office', 'created', 'updated')
     list_filter = ['mode', 'created', 'updated']
     search_fields = ['name']
+
 
 admin.site.register(Game, GameAdmin)
